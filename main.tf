bf7aec128bb5522b67dd25ece9c200b9a9869b30
maswanidev@gmail.com <maswanidev@gmail.com>
2023-11-26 21:50:55 +0530

    terraform

--- main.tf
+++ main.tf
@@ -14,11 +14,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "hosting_bucket" {
   bucket = var.bucket_name
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
 }
 
 ## Create a VPC
