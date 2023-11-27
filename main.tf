b51b1ddb7b49a43954b4f5d1611001bec3a92a40
maswanidev@gmail.com <maswanidev@gmail.com>
2023-11-27 11:14:12 +0530

    terraform

--- main.tf
+++ main.tf
@@ -16,6 +16,35 @@ resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_acl" "mybucket_acl" {
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_policy" "mybucket_policy" {
+  bucket = aws_s3_bucket.mybucket.id
+  policy = data.aws_iam_policy_document.mybucket_policy.json
+}
+
+data "aws_iam_policy_document" "mybucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["349018223180"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.mybucket.arn,
+      "${aws_s3_bucket.mybucket.arn}/*",
+    ]
+  }
+}
 ## Create a VPC
 #resource "aws_vpc" "example" {
 #  cidr_block = "10.0.0.0/16"
