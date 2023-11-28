e8a253a326ff9d902550317812e85d5f774c3af9
maswanidev@gmail.com <maswanidev@gmail.com>
2023-11-28 09:34:28 +0530

    makefile

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_s3_object" "object" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("./each.key")
+#  etag = filemd5("./each.key")
   acl = "public_read"
 }
 ## Create a VPC
